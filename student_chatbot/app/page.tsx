9d5826656110496b963f2098d7979fecb2767bc8
aryanprasher262004 <aryanprasher262004@gmail.com>
2026-02-18 02:41:36 +0530

    Next js setup with changed title , and home screen (welcome)

---
+++ student_chatbot/app/page.tsx
@@ -0,0 +1,41 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 dark:bg-black">
+   
+      <div
+        className="relative flex min-h-[860px] w-full max-w-8xl flex-col items-center justify-center rounded-xl bg-cover bg-c  bg-no-repeat shadow-2xl"
+        style={{ backgroundImage: "url('/bot.jpg')" }}
+      >
+        
+        <div className="absolute inset-0 bg-black/55 rounded-xl"></div>
+
+        {/* Content */}
+        <div className="relative z-10 flex flex-col items-center  gap-6 text-center px-6">
+          
+          <h1 className="text-5xl  font-bold text-white">
+             Student Help Chatbot
+          </h1>
+
+          <p className="max-w-md text-lg font-bold text-gray-200">
+            Your AI Assistant for Academics, Doubts, and Learning Support.
+          </p>
+          <p className="max-w-md pt-2 text-sm text-gray-200"> 
+            Created by Aryan Prasher, Anshika Yadav, Aditya Narayan, Itisha Panwar
+
+          </p>
+
+          <div className="flex flex-col gap-4 sm:flex-row">
+            <button className="rounded-full bg-white px-6 py-3 font-semibold text-black transition hover:bg-gray-200">
+              Start Chatting
+            </button>
+
+            <button className="rounded-full border border-white px-6 py-3 font-semibold text-white transition hover:bg-white hover:text-black">
+              Learn More
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
