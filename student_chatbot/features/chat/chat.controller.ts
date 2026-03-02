e76aa1a6e184be337cda69e91a806ec8442b59fe
aryanprasher262004 <aryanprasher262004@gmail.com>
2026-03-02 14:07:00 +0530

    added the response handeling service <chat.service.ts>

--- student_chatbot/features/chat/chat.controller.ts
+++ student_chatbot/features/chat/chat.controller.ts
@@ -1,15 +1,14 @@
 
 // handeles the chat requests and helps in enabling the chatbot to respond to the user queries.
 import { ChatRequest, ChatResponse } from "@/types/chat";
+import { processChat } from "./chat.service"; 
 
 export async function handleChat(
-    data: ChatRequest):
-    Promise<ChatResponse> {
+    data: ChatRequest): Promise<ChatResponse> 
+    {
         return {
-            reply: `You said: ${data.message}`,
+            reply: `You said ${data.message}`,
             intent: "mock_intent",
-            confidence: 1.0,
-        }
-
-    
-}
+            confidence: 1,
+        };
+    }
