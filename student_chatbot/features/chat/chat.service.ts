02c3f82a02d1f846f8f9bc4e9b7d708f58fc703a
aryanprasher262004 <aryanprasher262004@gmail.com>
2026-03-19 03:58:32 +0530

    added memory history context for handeling followup questions added @lib/memory.store.ts

--- student_chatbot/features/chat/chat.service.ts
+++ student_chatbot/features/chat/chat.service.ts
@@ -1,14 +1,38 @@
-  
-import { ChatRequest,ChatResponse
- } from "@/types/chat";
+import { ChatRequest, ChatResponse } from "@/types/chat";
+import { getAnswer } from "@/knowledge/knowledge.service";
+import { addMessage , getSessionHistory } from "@/lib/memory.store";
 
- export async function processChat(
-    data: ChatRequest):
-     Promise<ChatResponse> {
-        const reply = ` Your service is processed ${data.message} `;
-    return {
-        reply,
-        intent: "mock_intent",
-        confidence: 1,
-    };
- }
+export async function processChat(
+  data: ChatRequest
+): Promise<ChatResponse> {
+
+  // 🔴 TEMP MOCK data for testing
+  const mockIntent = "placement_query";
+
+  // store user message 
+  addMessage(data.sessionId, {
+    role: "user",
+    message: data.message,
+  });
+
+  // get answer 
+  
+
+  const answer = getAnswer(mockIntent);
+
+  // store bot response
+  addMessage(data.sessionId, {
+    role: "bot",
+    message: answer,
+  });
+
+  // history for context of previous question 
+  const history = getSessionHistory(data.sessionId);
+  console.log("Chat History ", history)
+
+  return {
+    reply: answer,
+    intent: mockIntent,
+    confidence: 1.0,
+  };
+}
